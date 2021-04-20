7ddf1fd92ed4d324b96583210025891adc9ed7d7
Hadi-ibrahim <ibrahim.a.hadi@hotmail.com>
2021-04-21 02:52:09 +0300

    Added solving quest end point

--- Server/routes/quests.js
+++ Server/routes/quests.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const db = require("../models");
 const verify = require("./verifyToken")
+const jwt = require("jsonwebtoken");
 
 Quests = db.Quest;
 Questions = db.Question;
 Puzzles = db.Puzzles;
 PuzzleOptions = db.PuzzleOptions;
+User = db.User;
 
 
 router.get("/", verify, async (req,res) => {
@@ -16,7 +18,73 @@ router.get("/", verify, async (req,res) => {
         res.status(400).send(err);
     }
 })
+router.post("/solve/:questId", verify, (req,res) => {
+    try {
+        id = req.params.questId;
+        token = req.header('auth-token');
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET); 
+        const userId = decoded.id;
+        Quests.findByPk(id,{attributes: {exclude:['createdAt', 'updatedAt', 'CategoryId']}})
+        .then((quest) => {
+            allQuestions = [];
+            allPuzzles = [];
 
+                Questions.findAll({
+                where: {
+                    QuestId : quest.id
+                },
+                attributes : {exclude: ["createdAt", "updatedAt","QuestId"]}
+            })
+            .then((questions) => {
+                questions.forEach(question => allQuestions.push(question));
+
+            });
+                Puzzles.findAll({
+                where:{
+                    QuestId: quest.id
+                },
+                attributes: {exclude: ["createdAt", "updatedAt","QuestId"]}
+            })
+            .then((puzzles)=> {
+                puzzles.forEach( puzzle => {
+                    if(puzzle.length!=0){
+                            PuzzleOptions.findAll({
+                            where: {
+                                PuzzleId: puzzle.id
+                            },
+                            attributes: {exclude: ["createdAt", "updatedAt","PuzzleId"]}
+                        })
+                        .then((options) => {
+                            allPuzzles.push ({
+                                Puzzle: {
+                                    prompt:puzzle,
+                                    Options: options
+                                }
+                            })
+
+
+                            if(areAnswersCorrect(allPuzzles, req.body.Puzzles, allQuestions, req.body.Questions) ){
+                                User.findByPk(userId)
+                                .then((usr) => {
+                                    usr.addCompletedQuest(id)
+                                    usr.update();
+                                    res.send("Quest completed successfully!")
+                                })
+                            }
+                            else res.send("Wrong answers. Try again");
+                            
+                        })
+                    }
+                });
+            })
+        });
+
+
+    } catch (err) {
+        res.status(400).send(err);
+        console.log(err);
+    }
+})
 router.get("/answers", verify, (req,res) => {
     try {
         Quests.findAll({attributes: {exclude:['createdAt', 'updatedAt', 'CategoryId']}})
@@ -68,13 +136,11 @@ router.get("/answers", verify, (req,res) => {
                         }
                     });
 
-
                 })
 
             });
 
         });
-
 
     } catch (err) {
         res.status(400).send(err);
@@ -139,6 +205,7 @@ router.get("/answers/:questId", verify, (req,res) => {
         console.log(err);
     }
 })
+
 router.get("/:questId", verify, async (req,res) => {
     id = req.params.questId;
     try {
@@ -148,5 +215,48 @@ router.get("/:questId", verify, async (req,res) => {
         res.status(400).send(err);
     }
 })
+
+function areAnswersCorrect(ansPuzzles, puzzles, ansQuestions, questions) {
+    ans = true
+    ansPuzzles.forEach((ansPuz) => {
+        const puz = puzzles.find(element => element.Puzzle.prompt.id == ansPuz.Puzzle.prompt.id)
+        if (!puz)
+            ans = false;
+
+        correctOptions = []
+        ansPuz.Puzzle.Options.forEach((ansOption) => {
+            if (ansOption.isCorrect)
+                correctOptions.push(ansOption.id);
+        })
+
+        selectedOptions = []; 
+
+        puz.Puzzle.Options.forEach((option) => {
+            selectedOptions.push(option.id);
+        })
+
+        const array2Sorted = selectedOptions.slice().sort();
+        
+        if (selectedOptions.length != correctOptions.length){
+            ans = false;
+        }
+
+        correctOptions.slice().sort().forEach( (value, index) =>{
+        if (value !== array2Sorted[index])
+            ans = false;
+        });
+        
+
+    })
+    ansQuestions.forEach((ansQuestion) => {
+        const ques = questions.find(question => question.id == ansQuestion.id);
+        if (!ques) 
+            ans = false
+
+        if(ques.solution !== ansQuestion.solution)
+            ans = false
+    })
+    return ans;
+}
 
 module.exports = router;
